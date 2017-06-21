8e14e674be9efa3e168733753b575b62cbba66ae
Luiz Carlos <mandukk25@gmail.com>
2017-06-21 20:09:07 -0300

    Emit newMessage on create new message

--- server/server.js
+++ server/server.js
@@ -14,14 +14,13 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
-    socket.emit('newMessage', {
-        from: 'Mandukk',
-        text: 'Hi, my name is Mandukk!',
-        createdAt: new Date().getTime()
-    });
-
     socket.on('createMessage', (message) => {
         console.log('Create message', message);
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        });
     })
     socket.on('disconnect', () => {
         console.log('User was disconnected');
