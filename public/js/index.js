8a1d5a527e2cc7bec988e415b52133f27d15cc1d
Luiz Carlos <mandukk25@gmail.com>
2017-06-21 20:27:21 -0300

    Greet new useser, and alert others

--- public/js/index.js
+++ public/js/index.js
@@ -8,9 +8,8 @@ socket.on('disconnect', function () {
     console.log('Disconnected from server');
 });
 
-socket.on ('newMessage', function (message) {
+socket.on('newMessage', function (message) {
     console.log('New message', message);
 });
-
 
 
